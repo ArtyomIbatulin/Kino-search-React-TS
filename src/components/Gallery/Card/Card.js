d09e6d9cf79f07c52bfe53318894db086c4bc384
Artyom Ibatulin <artem.alekseevich@bk.ru>
2021-03-05 14:26:44 +0300

    completed css jsx

--- src/components/Gallery/Card/Card.js
+++ src/components/Gallery/Card/Card.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import styles from './Card.module.css';
+import PropTypes from 'prop-types';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className={styles.card}>
-      <img src='https://upload.wikimedia.org/wikipedia/ru/7/7a/Spider-Man_3%2C_International_Poster.jpg'></img>
-      <p>Человек паук</p>
-      <p>Фантастика</p>
-      <p>Режиссер:</p>
-      <p>Описание:</p>
-      <p>Рейтинг: 5</p>
+      <img src={props.poster}></img>
+      <p>{props.name}</p>
+      <p>{props.genre}</p>
+      <p>{props.director}</p>
+      <p>{props.description}</p>
+      <p>{props.stars}</p>
     </div>
   );
+};
+
+Card.propTypes = {
+  poster: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  director: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  stars: PropTypes.string.isRequired,
 };
 
 export default Card;
