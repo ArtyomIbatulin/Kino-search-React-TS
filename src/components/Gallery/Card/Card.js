05982d44539c81ffbe614585bd9261621ef836fb
Artyom Ibatulin <artem.alekseevich@bk.ru>
2021-03-12 14:19:09 +0300

    commit before find a img

--- src/components/Gallery/Card/Card.js
+++ src/components/Gallery/Card/Card.js
@@ -2,16 +2,20 @@ import React from 'react';
 import styles from './Card.module.css';
 
 const Card = (props) => {
+  const { poster, name, genre, director, desc, description, rating } = props;
+
   return (
     <li className={styles.card}>
-      <img src={props.poster}></img>
-      <p>{props.name}</p>
-      <p>{props.genre}</p>
-      <p>{props.director}</p>
-      <p className={styles.desc} title={props.desc}>
-        {props.description}
+      <div className={styles.divImg}>
+        <img src={poster} className={styles.img}></img>
+      </div>
+      <p className={styles.pName}>{name}</p>
+      <p>{genre}</p>
+      <p>{director}</p>
+      <p className={styles.pDesc} title={desc}>
+        {description}
       </p>
-      <div>{props.rating}</div>
+      <div className={styles.rating}>{rating}</div>
     </li>
   );
 };
