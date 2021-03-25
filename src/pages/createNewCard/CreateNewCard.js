7bf95ec64a2b37d6ed0a30e441576b6220cbf222
Artyom Ibatulin <artem.alekseevich@bk.ru>
2021-03-25 18:09:40 +0300

    adding styles

--- src/pages/createNewCard/CreateNewCard.js
+++ src/pages/createNewCard/CreateNewCard.js
@@ -39,85 +39,100 @@ const CreateNewCard = () => {
 
   return (
     <main className={styles.createNewCard}>
-      <h1>Заполните карту!</h1>
-      <Formik
-        initialValues={{
-          poster: '',
-          name: '',
-          genre: '',
-          director: '',
-          desc: '',
-          description: {
-            href: '',
-            rel: 'noreferrer',
-            target: '_blank',
-            children: 'Описание',
-          },
-          stars: '',
-        }}
-        validationSchema={Yup.object({
-          poster: Yup.string()
-            .trim()
-            .url('Некорректный URL')
-            .required('Обязательное поле'),
-          name: Yup.string()
-            .trim()
-            .max(20, 'Не более 20 символов')
-            .required('Обязательное поле'),
-          genre: Yup.string()
-            .oneOf(
-              ['Боевики', 'Комедии', 'Фантастика', 'Ужасы'],
-              'Неправильный жанр'
-            )
-            .required('Обязательное поле'),
-          director: Yup.string()
-            .trim()
-            .max(20, 'Не более 20 символов')
-            .required('Обязательное поле'),
-          desc: Yup.string().trim().required('Обязательное поле'),
-          description: Yup.object().shape({
-            href: Yup.string()
+      <div className={styles.form}>
+        <h1>Заполните карту!</h1>
+        <Formik
+          initialValues={{
+            poster: '',
+            name: '',
+            genre: '',
+            director: '',
+            desc: '',
+            description: {
+              href: '',
+              rel: 'noreferrer',
+              target: '_blank',
+              children: 'Описание',
+            },
+            stars: '',
+          }}
+          validationSchema={Yup.object({
+            poster: Yup.string()
               .trim()
               .url('Некорректный URL')
               .required('Обязательное поле'),
-            rel: Yup.string(),
-            target: Yup.string(),
-            children: Yup.string(),
-          }),
-          stars: Yup.number().required('Обязательное поле').max(5),
-        })}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            newFilms = [...films, values];
-            dispatch(changeFilmsArray(newFilms));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        <Form>
-          <MyTextInput label="Постер" name="poster" type="text" />
-          <MyTextInput label="Название" name="name" type="text" />
-          <MyTextInput label="Режиссер" name="director" type="text" />
-          <MyTextInput label="Описание" name="desc" type="text" />
-          <MyTextInput label="Ссылка на фильм" name="description.href" />
-          <MyTextInput label="Рейтинг" name="stars" type="number" />
+            name: Yup.string()
+              .trim()
+              .max(20, 'Не более 20 символов')
+              .required('Обязательное поле'),
+            genre: Yup.string()
+              .oneOf(
+                ['Боевики', 'Комедии', 'Фантастика', 'Ужасы'],
+                'Неправильный жанр'
+              )
+              .required('Обязательное поле'),
+            director: Yup.string()
+              .trim()
+              .max(20, 'Не более 20 символов')
+              .required('Обязательное поле'),
+            desc: Yup.string().trim().required('Обязательное поле'),
+            description: Yup.object().shape({
+              href: Yup.string()
+                .trim()
+                .url('Некорректный URL')
+                .required('Обязательное поле'),
+              rel: Yup.string(),
+              target: Yup.string(),
+              children: Yup.string(),
+            }),
+            stars: Yup.number().required('Обязательное поле').max(5),
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              newFilms = [...films, values];
+              dispatch(changeFilmsArray(newFilms));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          <Form>
+            <MyTextInput label="Постер" name="poster" type="text" />
+            <MyTextInput label="Название" name="name" type="text" />
+            <MyTextInput label="Режиссер" name="director" type="text" />
+            <MyTextInput label="Описание" name="desc" type="text" />
+            <MyTextInput label="Ссылка на фильм" name="description.href" />
+            <MyTextInput label="Рейтинг" name="stars" type="number" />
 
-          <MySelect label="Жанр" name="genre">
-            <option value="">Выберите жанр фильма</option>
-            <option value="Боевики">Боевики</option>
-            <option value="Комедии">Комедии</option>
-            <option value="Фантастика">Фантастика</option>
-            <option value="Ужасы">Ужасы</option>
-          </MySelect>
+            <MySelect label="Жанр" name="genre">
+              <option value="">Выберите жанр фильма</option>
+              <option value="Боевики">Боевики</option>
+              <option value="Комедии">Комедии</option>
+              <option value="Фантастика">Фантастика</option>
+              <option value="Ужасы">Ужасы</option>
+            </MySelect>
 
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
-      <Link to="/" className={styles.link}>
-        Вернуться на главную
-      </Link>
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+        <Link to="/" className={styles.link}>
+          Вернуться на главную
+        </Link>
+      </div>
     </main>
   );
 };
 
 export default CreateNewCard;
+
+// {
+// https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/cc26467c-bc40-410e-8536-65667a902946/220x330
+// https://www.kinopoisk.ru/film/841700/
+// https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/6137a4d9-3ec2-4a8d-8881-a45a6beab8be/800x800
+//   "poster": "https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/6137a4d9-3ec2-4a8d-8881-a45a6beab8be/800x800",
+//   "name": "Такси",
+//   "genre": "comedy",
+//   "director": "ЖП",
+//   "desc": "Молодой таксист Даниэль помешан на быстрой езде. Как ураган, проносится он по извилистым улицам Марселя на своём мощном ревущем звере «Пежо», пугая пассажиров и прохожих. Неподкупный полицейский Эмильен вынуждает его помочь в поимке банды грабителей, ускользающих от полиции на своих неуловимых «Мерседесах». И до самого конца не ясно, кто же сможет удержаться на крутом вираже.",
+//   "description": "https://www.kinopoisk.ru/film/14349/",
+//   "stars": 4
+// }
