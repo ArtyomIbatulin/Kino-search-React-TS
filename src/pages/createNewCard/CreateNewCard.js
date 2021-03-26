fd6052d84e0770e61921a297454326f86e8bcded
Artyom Ibatulin <artem.alekseevich@bk.ru>
2021-03-26 18:27:11 +0300

    need fix app

--- src/pages/createNewCard/CreateNewCard.js
+++ src/pages/createNewCard/CreateNewCard.js
@@ -8,24 +8,26 @@ import { Link } from 'react-router-dom';
 
 const CreateNewCard = () => {
   const dispatch = useDispatch();
-  let newFilms = [];
+  // let newFilms = [];
   const films = useSelector((state) => state.films);
 
   const MyTextInput = ({ label, ...props }) => {
     const [field, meta] = useField(props);
+
     return (
-      <>
+      <div className={styles.formControl}>
         <label htmlFor={props.id || props.name}>{label}</label>
         <input {...field} {...props} />
         {meta.touched && meta.error ? (
           <div className={styles.error}>{meta.error}</div>
         ) : null}
-      </>
+      </div>
     );
   };
 
   const MySelect = ({ label, ...props }) => {
     const [field, meta] = useField(props);
+
     return (
       <>
         <label htmlFor={props.id || props.name}>{label}</label>
@@ -39,100 +41,93 @@ const CreateNewCard = () => {
 
   return (
     <main className={styles.createNewCard}>
-      <div className={styles.form}>
-        <h1>Заполните карту!</h1>
-        <Formik
-          initialValues={{
-            poster: '',
-            name: '',
-            genre: '',
-            director: '',
-            desc: '',
-            description: {
-              href: '',
-              rel: 'noreferrer',
-              target: '_blank',
-              children: 'Описание',
-            },
-            stars: '',
-          }}
-          validationSchema={Yup.object({
-            poster: Yup.string()
+      <h1>Заполните карту!</h1>
+      <Formik
+        initialValues={{
+          poster: '',
+          name: '',
+          genre: '',
+          director: '',
+          desc: '',
+          description: {
+            href: '',
+            rel: 'noreferrer',
+            target: '_blank',
+            children: 'Описание',
+          },
+          stars: '',
+        }}
+        validationSchema={Yup.object({
+          poster: Yup.string()
+            .trim()
+            .url('Некорректный URL')
+            .required('Обязательное поле'),
+          name: Yup.string()
+            .trim()
+            .max(20, 'Не более 20 символов')
+            .required('Обязательное поле'),
+          genre: Yup.string()
+            .oneOf(
+              ['Боевики', 'Комедии', 'Фантастика', 'Ужасы'],
+              'Неправильный жанр'
+            )
+            .required('Обязательное поле'),
+          director: Yup.string()
+            .trim()
+            .max(20, 'Не более 20 символов')
+            .required('Обязательное поле'),
+          desc: Yup.string().trim().required('Обязательное поле'),
+          description: Yup.object().shape({
+            href: Yup.string()
               .trim()
               .url('Некорректный URL')
               .required('Обязательное поле'),
-            name: Yup.string()
-              .trim()
-              .max(20, 'Не более 20 символов')
-              .required('Обязательное поле'),
-            genre: Yup.string()
-              .oneOf(
-                ['Боевики', 'Комедии', 'Фантастика', 'Ужасы'],
-                'Неправильный жанр'
-              )
-              .required('Обязательное поле'),
-            director: Yup.string()
-              .trim()
-              .max(20, 'Не более 20 символов')
-              .required('Обязательное поле'),
-            desc: Yup.string().trim().required('Обязательное поле'),
-            description: Yup.object().shape({
-              href: Yup.string()
-                .trim()
-                .url('Некорректный URL')
-                .required('Обязательное поле'),
-              rel: Yup.string(),
-              target: Yup.string(),
-              children: Yup.string(),
-            }),
-            stars: Yup.number().required('Обязательное поле').max(5),
-          })}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              newFilms = [...films, values];
-              dispatch(changeFilmsArray(newFilms));
-              setSubmitting(false);
-            }, 400);
-          }}
-        >
-          <Form>
-            <MyTextInput label="Постер" name="poster" type="text" />
-            <MyTextInput label="Название" name="name" type="text" />
-            <MyTextInput label="Режиссер" name="director" type="text" />
-            <MyTextInput label="Описание" name="desc" type="text" />
-            <MyTextInput label="Ссылка на фильм" name="description.href" />
-            <MyTextInput label="Рейтинг" name="stars" type="number" />
+            rel: Yup.string(),
+            target: Yup.string(),
+            children: Yup.string(),
+          }),
+          stars: Yup.number().required('Обязательное поле').max(5),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          // setTimeout(() => {
+          // const newFilms = [...films, values];
+          dispatch(changeFilmsArray([...films, values]));
+          setSubmitting(false);
+          // resetForm();
+          alert('Карта создана');
+          // }, 400);
+        }}
+      >
+        <Form>
+          <MyTextInput label="Постер" name="poster" type="text" />
+          <MyTextInput label="Название" name="name" type="text" />
+          <MyTextInput label="Режиссер" name="director" type="text" />
+          <MyTextInput label="Описание" name="desc" type="text" />
+          <MyTextInput label="Ссылка на фильм" name="description.href" />
+          <MyTextInput label="Рейтинг" name="stars" type="number" />
 
-            <MySelect label="Жанр" name="genre">
-              <option value="">Выберите жанр фильма</option>
-              <option value="Боевики">Боевики</option>
-              <option value="Комедии">Комедии</option>
-              <option value="Фантастика">Фантастика</option>
-              <option value="Ужасы">Ужасы</option>
-            </MySelect>
+          <MySelect className={styles.select} name="genre">
+            <option value="">Выберите жанр фильма</option>
+            <option value="Боевики">Боевики</option>
+            <option value="Комедии">Комедии</option>
+            <option value="Фантастика">Фантастика</option>
+            <option value="Ужасы">Ужасы</option>
+          </MySelect>
 
-            <button type="submit">Submit</button>
-          </Form>
-        </Formik>
-        <Link to="/" className={styles.link}>
-          Вернуться на главную
-        </Link>
-      </div>
+          <div className={styles.btns}>
+            <button type="submit">Создать карту</button>
+            <button type="reset">Очистить</button>
+          </div>
+        </Form>
+      </Formik>
+      <Link to="/" className={styles.link}>
+        Вернуться на главную
+      </Link>
     </main>
   );
 };
 
 export default CreateNewCard;
 
-// {
-// https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/cc26467c-bc40-410e-8536-65667a902946/220x330
+// https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/6f631486-e947-487d-94d6-41c2b5a8f5a0/300x450
 // https://www.kinopoisk.ru/film/841700/
-// https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/6137a4d9-3ec2-4a8d-8881-a45a6beab8be/800x800
-//   "poster": "https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/6137a4d9-3ec2-4a8d-8881-a45a6beab8be/800x800",
-//   "name": "Такси",
-//   "genre": "comedy",
-//   "director": "ЖП",
-//   "desc": "Молодой таксист Даниэль помешан на быстрой езде. Как ураган, проносится он по извилистым улицам Марселя на своём мощном ревущем звере «Пежо», пугая пассажиров и прохожих. Неподкупный полицейский Эмильен вынуждает его помочь в поимке банды грабителей, ускользающих от полиции на своих неуловимых «Мерседесах». И до самого конца не ясно, кто же сможет удержаться на крутом вираже.",
-//   "description": "https://www.kinopoisk.ru/film/14349/",
-//   "stars": 4
-// }
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -7,37 +7,53 @@ const getSearchText = (state) => state.searchText;
 export const filteredData = createSelector(
   [getFilms, getradioSelect, getSearchText],
   (films, radioSelect, searchText) => {
-    let res;
-    switch (radioSelect) {
-      case 'all':
-        res = films;
-        break;
-      case 'action_movie':
-        res = films.filter(
-          (item) => item.genre === 'Боевики' || item.genre === 'action_movie'
-        );
-        break;
-      case 'comedy':
-        res = films.filter(
-          (item) => item.genre === 'Комедии' || item.genre === 'comedy'
-        );
-        break;
-      case 'fantasy':
-        res = films.filter(
-          (item) => item.genre === 'Фантастика' || item.genre === 'fantasy'
-        );
-        break;
-      case 'horror':
-        res = films.filter(
-          (item) => item.genre === 'Ужасы' || item.genre === 'horror'
-        );
-        break;
-    }
+    const genreSelector = {
+      action_movie: 'Боевики',
+      comedy: 'Комедии',
+      fantasy: 'Фантастика',
+      horror: 'Ужасы',
+    };
+    // let res;
+    // switch (radioSelect) {
+    //   case 'all':
+    //     res = films;
+    //     break;
+    //   case 'action_movie':
+    //     res = films.filter(
+    //       (item) => item.genre === 'Боевики' || item.genre === 'action_movie'
+    //     );
+    //     break;
+    //   case 'comedy':
+    //     res = films.filter(
+    //       (item) => item.genre === 'Комедии' || item.genre === 'comedy'
+    //     );
+    //     break;
+    //   case 'fantasy':
+    //     res = films.filter(
+    //       (item) => item.genre === 'Фантастика' || item.genre === 'fantasy'
+    //     );
+    //     break;
+    //   case 'horror':
+    //     res = films.filter(
+    //       (item) => item.genre === 'Ужасы' || item.genre === 'horror'
+    //     );
+    //     break;
+    // }
 
-    return res.filter(
-      (item) =>
-        item.name.toLowerCase().includes(searchText.toLowerCase()) ||
-        item.director.toLowerCase().includes(searchText.toLowerCase())
-    );
+    return films
+      .filter((item) =>
+        radioSelect === 'all' ? true : item.genre === genreSelector[radioSelect]
+      )
+      .filter(
+        (item) =>
+          item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+          item.director.toLowerCase().includes(searchText.toLowerCase())
+      );
+
+    // return res.filter(
+    //   (item) =>
+    //     item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+    //     item.director.toLowerCase().includes(searchText.toLowerCase())
+    // );
   }
 );
