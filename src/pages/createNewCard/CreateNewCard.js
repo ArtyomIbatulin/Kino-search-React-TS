d1749dcf04f8fcdbe41507ec29b4d31cc6a5ed90
Artyom Ibatulin <artem.alekseevich@bk.ru>
2021-03-29 12:30:35 +0300

    ready to fix

--- src/pages/createNewCard/CreateNewCard.js
+++ src/pages/createNewCard/CreateNewCard.js
@@ -5,12 +5,9 @@ import styles from './CreateNewCard.module.scss';
 import { changeFilmsArray } from '../../store/actions';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-// import { ThemeProvider, theme } from '@chakra-ui/react';
-// import ChakraInpit from '../../components/chakraInput/ChakraInpit';
 
 const CreateNewCard = () => {
   const dispatch = useDispatch();
-  // let newFilms = [];
   const films = useSelector((state) => state.films);
 
   const MyTextInput = ({ label, ...props }) => {
@@ -42,13 +39,8 @@ const CreateNewCard = () => {
   };
 
   return (
-    // <ThemeProvider theme={theme}>
     <main className={styles.createNewCard}>
-<<<<<<< HEAD
       <h1 className={styles.h1}>Заполните карту!</h1>
-=======
-      <h1>Заполните карту!</h1>
->>>>>>> review
       <Formik
         initialValues={{
           poster: '',
@@ -86,10 +78,7 @@ const CreateNewCard = () => {
           desc: Yup.string().trim().required('Обязательное поле'),
           description: Yup.object().shape({
             href: Yup.string()
-<<<<<<< HEAD
-=======
               .trim()
->>>>>>> review
               .url('Некорректный URL')
               .required('Обязательное поле'),
             rel: Yup.string(),
@@ -99,39 +88,21 @@ const CreateNewCard = () => {
           stars: Yup.number().required('Обязательное поле').max(5),
         })}
         onSubmit={(values, { setSubmitting }) => {
-<<<<<<< HEAD
-          setTimeout(() => {
-            newFilms = [...films, values];
-            dispatch(changeFilmsArray(newFilms));
-            setSubmitting(false);
-          }, 400);
-=======
-          // setTimeout(() => {
-          // const newFilms = [...films, values];
           dispatch(changeFilmsArray([...films, values]));
           setSubmitting(false);
           // resetForm();
           alert('Карта создана');
-          // }, 400);
->>>>>>> review
         }}
       >
         <Form>
           <MyTextInput label="Постер" name="poster" type="text" />
           <MyTextInput label="Название" name="name" type="text" />
-<<<<<<< HEAD
-=======
           <MyTextInput label="Режиссер" name="director" type="text" />
->>>>>>> review
           <MyTextInput label="Описание" name="desc" type="text" />
           <MyTextInput label="Ссылка на фильм" name="description.href" />
           <MyTextInput label="Рейтинг" name="stars" type="number" />
 
-<<<<<<< HEAD
-          <MySelect label="Жанр" name="genre">
-=======
           <MySelect className={styles.select} name="genre">
->>>>>>> review
             <option value="">Выберите жанр фильма</option>
             <option value="Боевики">Боевики</option>
             <option value="Комедии">Комедии</option>
@@ -139,21 +110,16 @@ const CreateNewCard = () => {
             <option value="Ужасы">Ужасы</option>
           </MySelect>
 
-<<<<<<< HEAD
-          <button type="submit">Submit</button>
-=======
           <div className={styles.btns}>
             <button type="submit">Создать карту</button>
             <button type="reset">Очистить</button>
           </div>
->>>>>>> review
         </Form>
       </Formik>
       <Link to="/" className={styles.link}>
         Вернуться на главную
       </Link>
     </main>
-    // </ThemeProvider>
   );
 };
 
