c4ab9e6f416ef488473f26d38364f307ff85ee55
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2023-03-13 17:59:03 +0300

    renamed validation file

--- src/pages/createNewCard/validation.ts
+++ src/pages/createNewCard/validation.ts
@@ -7,14 +7,14 @@ const validation = Yup.object({
     .required('Обязательное поле'),
   name: Yup.string()
     .trim()
-    .max(20, 'Не более 20 символов')
+    .max(20, 'He более 20 символов')
     .required('Обязательное поле'),
   genre: Yup.string()
     .oneOf(['Боевики', 'Комедии', 'Фантастика', 'Ужасы'], 'Неправильный жанр')
     .required('Обязательное поле'),
   director: Yup.string()
     .trim()
-    .max(20, 'Не более 20 символов')
+    .max(20, 'He более 20 символов')
     .required('Обязательное поле'),
   desc: Yup.string().trim().required('Обязательное поле'),
   description: Yup.string()
