b60bbba221e5cdbac8ffca1523dacfa57b5f4dcf
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2023-04-15 21:35:15 +0300

    fixed filmstype

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -3,14 +3,13 @@ import { films } from '../components/App/db';
 import {ActionsTypes} from './actions'
 
 export type FilmsType = {
-  poster: string | null,
-  name:  string | null,
-  genre:  string | null,
-  director: string | null,
-  desc:  string | null,
-  description: string | null,
-  stars: number | null,
-  rating:  string | null,
+  poster: string,
+  name:  string,
+  genre:  string,
+  director: string,
+  desc:  string,
+  description: string,
+  stars: number
 }
 
 export type InitialStateType = {
